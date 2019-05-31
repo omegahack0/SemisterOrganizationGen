5d10f4fafebada4b8ae0d41b2d31954053f3ba47
omegahack0 <32969902+omegahack0@users.noreply.github.com>
2019-05-31 12:51:54 -0400

    Add files via upload

---
+++ Determines Semester.ps1
@@ -0,0 +1,14 @@
+﻿##Determines the Semester your in
+$dayOfYear = (Get-Date).dayofyear
+
+$Spring = 14, 116
+$Summer = 133, 222
+$Fall = 239, 349
+
+if ($dayOfYear -In $Spring[0]..$Spring[1]){
+Write-Host "Spring Semester"
+}elseif ($dayOfYear -In $Summer[0]..$Summer[1]){
+Write-Host "Summer Semester"
+}elseif ($dayOfYear -In $Fall[0]..$Fall[1]){
+Write-Host "Fall Semester"
+}
